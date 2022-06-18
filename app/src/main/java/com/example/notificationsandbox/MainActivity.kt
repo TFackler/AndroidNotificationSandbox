399bba634067cb2adcc0adf827eeba450616d537
TFackler <38538728+TFackler@users.noreply.github.com>
2022-06-18 06:21:49 +0200

    :lipstick: Add buttons to main UI

--- app/src/main/java/com/example/notificationsandbox/MainActivity.kt
+++ app/src/main/java/com/example/notificationsandbox/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.notificationsandbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
